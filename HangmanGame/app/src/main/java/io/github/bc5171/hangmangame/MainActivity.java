bbde8df893163c83de566c07f12b4e77cbca23ab
Benjamin Confer <bconfer3@gatech.edu>
2017-10-20 13:16:13 -0400

    Initial commit.

---
+++ HangmanGame/app/src/main/java/io/github/bc5171/hangmangame/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.bc5171.hangmangame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
