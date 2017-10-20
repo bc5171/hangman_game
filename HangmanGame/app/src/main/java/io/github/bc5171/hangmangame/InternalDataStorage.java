b8c668d22067f31194803ea0bf34ddb5399f991a
Benjamin Confer <bconfer3@gatech.edu>
2017-10-20 16:56:46 -0400

    Created DB setup for app.

--- HangmanGame/app/src/main/java/io/github/bc5171/hangmangame/InternalDataStorage.java
+++ HangmanGame/app/src/main/java/io/github/bc5171/hangmangame/InternalDataStorage.java
@@ -1,7 +1,9 @@
 package io.github.bc5171.hangmangame;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.*;
+
+import java.sql.SQLData;
 
 /**
  * Created by Ben on 10/20/2017.
@@ -9,14 +11,58 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class InternalDataStorage extends SQLiteOpenHelper {
 
+    // DB Name
     private static final String DATABASE_NAME = "hangmangame";
 
+    // DB Version
     private static final int DATABASE_VERSION = 1;
+
+    // DB Table: words
+    public static final String table_WORDS = "words";
+    public static final String words_ID = "id";
+    public static final String words_WORD = "word";
+
+    // DB Table: user
+    public static final String table_USER = "user";
+    public static final String user_ID = "id";
+    public static final String user_OnlineID = "online_id";
+    public static final String user_TotSolved = "totSolved";
+    public static final String user_TotAttempted = "totAttempted";
+
+    // DB Table: saved
+    public static final String table_SAVED = "saved";
+    public static final String saved_ID = "id";
+    public static final String saved_Word = "saved_word";
+    public static final String saved_Guess = "saved_guess";
 
     public InternalDataStorage(Context context) {
         super (context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    
+    @Override
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + table_USER);
+        db.execSQL("DROP TABLE IF EXISTS " + table_SAVED);
+        db.execSQL("DROP TABLE IF EXISTS " + table_WORDS);
+        onCreate(db);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + table_USER + " (" +
+            user_ID + " PRIMARY KEY AUTOINCREMENT, " +
+            user_OnlineID + " TEXT, " +
+            user_TotAttempted + " INT, " +
+            user_TotSolved + " INT)");
+
+        db.execSQL("CREATE TABLE " + table_WORDS + " (" +
+            words_ID + " PRIMARY KEY AUTOINCREMENT, " +
+            words_WORD + " TEXT)");
+
+        db.execSQL("CREATE TABLE " + table_SAVED + " (" +
+            saved_ID + " PRIMARY KEY AUTOINCREMENT, " +
+            saved_Guess + " TEXT, " +
+            saved_Word + "TEXT)");
+    }
 
 }
